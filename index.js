9e6b651234a1a615f2711ac48dfdf74a851040a9
Gerald12344 <harvey.randall@aol.com>
2021-05-11 21:57:41 +0100

    f

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+'use strict';
 var screenshot = require('desktop-screenshot');
-var fs = require("fs"),
-    PNG = require("pngjs").PNG;
 
 
+let Jimp = require('jimp')
 let iterations = 0;
+
+const ioHook = require('iohook');
+
+let clicks = 0
+ioHook.start();
 
 const robot = require("robotjs");
 const {
@@ -16,39 +21,31 @@ function Image() {
     screenshot("screenshot.png", {
         screen: 'desktop',
         fast: true,
-        quality: 1,
         width: 400
     }, function (error, complete) {
         if (error) {
             console.log("Screenshot failed", error);
         } else {
-
-            console.log("Screenshot succeeded");
-
-
-            fs.createReadStream('screenshot.png')
-                .pipe(
-                    new PNG({
-                        filterType: 4,
-                    })
-                )
-                .on("parsed", function () {
-                    let iterations2 = 0
-                    let clickedx = [0]
-                    let clickedy = [0]
-                    for (var y = 0; y < this.height; y++) {
-                        for (var x = 0; x < this.width; x++) {
-                            var idx = (this.width * y + x) << 2;
-                            // invert color
-                            //color 255,219,195
-                            if (this.data[idx] == 255 && this.data[idx + 1] == 219 && this.data[idx + 2] == 195) {
+            let iterations2 = 0
+            let clickedx = [0]
+            let clickedy = [0]
+            Jimp.read('screenshot.png')
+                .then(image => {
+                    image.crop(104, 30, 186, 181)
+                    //image.write('output.png')
+                    //console.log(image.bitmap)
+                    for (var y = 0; y < image.bitmap.height; y++) {
+                        for (var x = 0; x < image.bitmap.width; x++) {
+                            var idx = (image.bitmap.width * y + x) << 2;
+                            if (image.bitmap.data[idx] == 255 && image.bitmap.data[idx + 1] == 219 && image.bitmap.data[idx + 2] == 195) {
+                                //console.log(image.bitmap.data[idx] +" "+image.bitmap.data[idx + 1]+" "+image.bitmap.data[idx + 2])
                                 let included = false
-                                for (i = 0; i < clickedx.length; i++) {
+                                for (let i = 0; i < clickedx.length; i++) {
                                     if (((clickedx[i] - 5 < x && clickedx[i] + 5 > x) || (clickedy[i] - 5 < y && clickedy[i] + 5 > y))) {
                                         included = true
                                     }
                                 }
-                                if(included == false){
+                                if (included == false) {
                                     clickedx.push(x)
                                     clickedy.push(y)
                                     if (iterations2 > 10) {
@@ -57,24 +54,104 @@ function Image() {
                                     }
                                     iterations2++
                                     //console.log(screenSize)
-                                    let newx = (Math.random() * 30 +(((x / 400) * 1366) - 30)) + 20
-                                    let newy = (Math.random() * 30 +(((y / 224) * 768) - 30)) + 20
-                                    console.log(`Location x:${newx} and y:${newy}`)
+                                    //console.log(image.bitmap.width)
+                                    /*
+                                    let newx = (x / 186) * 3.416 + 355
+                                    let newy = (y / 181) * 3.42857143 + 103
+                                    */
+                                    let newx = (x ) * 3.416 + 355
+                                    let newy = (y ) * 3.42857143 + 103
+                                    //let newx = (Math.random() * 30 + (((x / 400) * 1366) - 30)) + 20
+                                    //let newy = (Math.random() * 30 + (((y / 224) * 768) - 30)) + 20
+                                    //console.log(`Location x:${newx} and y:${newy}`)
+                                    clicks++
                                     robot.moveMouse(newx, newy)
                                     robot.mouseClick()
+                                }
+                
+                            }
+                        }
+                    }
+                    if (iterations > -1) {
+                        //console.log("times run:" + iterations)
+                        Image()
+                        //setTimeout(Image, 10)
+                        iterations++
+                    }
+                })
+                /*
+
+            fs.createReadStream('screenshot.png')
+                .pipe(
+                    new PNG({
+                        filterType: 4,
+                    })
+                )
+                //355 103
+                .on("parsed", function () {
+
+
+                    //this.crop(355,103,639,594)
+                    for (var y = 0; y < this.height; y++) {
+                        for (var x = 0; x < this.width; x++) {
+                            var idx = (this.width * y + x) << 2;
+                            if (this.data[idx] == 255 && this.data[idx + 1] == 219 && this.data[idx + 2] == 195) {
+                                let included = false
+                                for (let i = 0; i < clickedx.length; i++) {
+                                    if (((clickedx[i] - 5 < x && clickedx[i] + 5 > x) || (clickedy[i] - 5 < y && clickedy[i] + 5 > y))) {
+                                        included = true
+                                    }
+                                }
+                                if (included == false) {
+                                    clickedx.push(x)
+                                    clickedy.push(y)
+                                    if (iterations2 > 10) {
+                                        iterations = 10
+                                        return process.exit(22);
+                                    }
+                                    iterations2++
+                                    //console.log(screenSize)
+                                    let newx = (x / 400) * 1366
+                                    let newy = (y / 224) * 768
+                                    //let newx = (Math.random() * 30 + (((x / 400) * 1366) - 30)) + 20
+                                    //let newy = (Math.random() * 30 + (((y / 224) * 768) - 30)) + 20
+                                    //console.log(`Location x:${newx} and y:${newy}`)
+                                    clicks++
+                                    //robot.moveMouse(newx, newy)
+                                    //robot.mouseClick()
                                 }
                             }
                         }
                     }
-                    if (iterations < 45) {
-                        console.log(iterations)
+                    if (iterations > -1) {
+                        //console.log("times run:" + iterations)
                         Image()
                         //setTimeout(Image, 10)
                         iterations++
                     }
 
-                });
+                }); */
         }
+       
     });
 }
 Image()
+
+ioHook.on("keydown", event => {
+    if (event.altKey && event.keycode == 35) {
+        iterations = -1
+        console.log("Program Halted.")
+    }
+    if (event.altKey && event.keycode == 34) {
+        iterations = 1
+        console.log("Program continued.")
+        Image()
+    }
+});
+
+setInterval(function () {
+    console.log("Main FUnction: " + iterations / 5)
+    console.log("Clicks per second: " + clicks / 5)
+    iterations = 0
+    clicks = 0
+}, 5000)
